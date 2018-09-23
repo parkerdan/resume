7c45f96fc460272c42d02154c3d869486d535a99
Dan Parker <danparker@Dans-MacBook-Pro.local>
2018-09-22 22:51:28 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -3,279 +3,306 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite/no-important';
 
-import Animated from 'animated/lib/targets/react-dom';
+import Atom from './assets/images/atom.svg';
+import AWSS3 from './assets/images/aws-s3.svg';
+import AWS from './assets/images/aws.svg';
+import Codeship from './assets/images/codeship.svg';
+import CSS3 from './assets/images/css-3.svg';
+import Electron from './assets/images/electron.svg';
+import ES6 from './assets/images/es6.svg';
+import ESLint from './assets/images/eslint.svg';
+import Expo from './assets/images/expo.svg';
+import Express from './assets/images/express.svg';
+import Flow from './assets/images/flow.svg';
+import Flux from './assets/images/flux.svg';
+import Git from './assets/images/git-icon.svg';
+import GoogleCloud from './assets/images/google-cloud.svg';
+import GraphQL from './assets/images/graphql.svg';
+import Heroku from './assets/images/heroku.svg';
+import HTML5 from './assets/images/html-5.svg';
+import Javascript from './assets/images/javascript.svg';
+import Moment from './assets/images/momentjs.svg';
+import MongoDB from './assets/images/mongodb.svg';
+import Mysql from './assets/images/mysql.svg';
+import Node from './assets/images/node.svg';
+import Nodemon from './assets/images/nodemon.svg';
+import NPM from './assets/images/npm.svg';
+import Parse from './assets/images/parse.svg';
+import Postgresql from './assets/images/postgresql.svg';
+import Postman from './assets/images/postman.svg';
+import Rails from './assets/images/rails.svg';
+import ReactRouter from './assets/images/react-router.svg';
+import ReactSVG from './assets/images/react.svg';
+import Realm from './assets/images/realm.svg';
+// import ReasonML from './assets/images/reasonml.svg';
+import Redis from './assets/images/redis.svg';
+import Redux from './assets/images/redux.svg';
+import Ruby from './assets/images/ruby.svg';
+import Rubygems from './assets/images/rubygems.svg';
+import Sass from './assets/images/sass.svg';
+import Sentry from './assets/images/sentry.svg';
+import Sequelize from './assets/images/sequelize.svg';
+import Serverless from './assets/images/serveless.svg';
+import Sinatra from './assets/images/sinatra.svg';
+import Sketch from './assets/images/sketch.svg';
+import SocketIO from './assets/images/socket.io.svg';
+import StackOverflow from './assets/images/stackoverflow.svg';
+import Stripe from './assets/images/stripe.svg';
+import Twilio from './assets/images/twilio.svg';
+import Webpack from './assets/images/webpack.svg';
+import Yarn from './assets/images/yarn.svg';
 
-import ComputerDude from './assets/computerDude.png';
-import Macbook from './assets/macbook.jpg';
-import Project from './Project';
 
-import {Colors} from './constants';
+import Macbook from './assets/images/macbook.jpg';
+import './assets/globals.css';
+import Github from './assets/images/github.svg';
+import LinkedIn from './assets/images/linkedIn.svg';
+import Medium from './assets/images/medium.svg';
+import Resume from './assets/images/resume.svg';
+
+const links = [
+  {
+    alt: 'Medium',
+    href: 'https://medium.com/@parkerdan',
+    src: Medium,
+  },
+  {
+    alt: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/parkerdanielo/',
+    src: LinkedIn,
+  },
+  {
+    alt: 'GitHub',
+    href: 'https://github.com/parkerdan',
+    src: Github,
+  },
+];
+
+const skills = [
+  {src: Javascript, alt: 'Javascript'},
+  {src: ReactSVG, alt: 'React'},
+  {src: GraphQL, alt: 'GraphQL'},
+  {src: Flow, alt: 'Flow'},
+  {src: Postman, alt: 'Postman'},
+  {src: Moment, alt: 'Moment'},
+  {src: Yarn, alt: 'Yarn'},
+
+  {src: Atom, alt: 'Atom'},
+  {src: AWSS3, alt: 'AWSS3'},
+  {src: AWS, alt: 'AWS'},
+  {src: Codeship, alt: 'Codeship'},
+  {src: CSS3, alt: 'CSS3'},
+  {src: Electron, alt: 'Electron'},
+  {src: ES6, alt: 'ES6'},
+  {src: ESLint, alt: 'ESLint'},
+  {src: Expo, alt: 'Expo'},
+  {src: Express, alt: 'Express'},
+  {src: Flux, alt: 'Flux'},
+  {src: Git, alt: ''},
+  {src: GoogleCloud, alt: 'GoogleCloud'},
+  {src: Heroku, alt: 'Heroku'},
+  {src: HTML5, alt: 'HTML5'},
+  {src: MongoDB, alt: 'MongoDB'},
+  {src: Mysql, alt: 'Mysql'},
+  {src: Node, alt: 'Node'},
+  {src: Nodemon, alt: 'Nodemon'},
+  {src: NPM, alt: 'NPM'},
+  {src: Parse, alt: 'Parse'},
+  {src: Postgresql, alt: 'Postgresql'},
+  {src: Rails, alt: 'Rails'},
+  {src: ReactRouter, alt: 'ReactRouter'},
+  {src: Realm, alt: 'Realm'},
+  // {src: ReasonML, alt: 'ReasonML'},
+  {src: Redis, alt: 'Redis'},
+  {src: Redux, alt: 'Redux'},
+  {src: Ruby, alt: 'Ruby'},
+  {src: Rubygems, alt: 'Rubygems'},
+  {src: Sass, alt: 'Sass'},
+  {src: Sentry, alt: 'Sentry'},
+  {src: Sequelize, alt: 'Sequelize'},
+  {src: Serverless, alt: 'Serverless'},
+  {src: Sinatra, alt: 'Sinatra'},
+  {src: Sketch, alt: 'Sketch'},
+  {src: SocketIO, alt: 'SocketIO'},
+  {src: StackOverflow, alt: 'StackOverflow'},
+  {src: Stripe, alt: 'Stripe'},
+  {src: Twilio, alt: 'Twilio'},
+  {src: Webpack, alt: 'Webpack'},
+]
+
+function Links() {
+  return (
+    <React.Fragment>
+      {links.map(link => (
+        <a key={link.alt} href={link.href} target="_blank">
+          <img alt={link.alt} src={link.src} className={css(styles.imageLarge)}/>
+        </a>
+      ))}
+      <a href="https://s3-us-west-1.amazonaws.com/parker-resume/parker.pdf" target="_blank">
+        <img alt={'resume'} src={Resume} className={css(styles.imageLarge)}/>
+      </a>
+    </React.Fragment>
+  )
+}
+
+function Skills() {
+  return skills.map(skill => (
+    <img
+      alt={skill.alt}
+      key={skill.alt}
+      src={skill.src}
+      className={css(styles.imageSmall)}
+    />
+  ));
+}
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.contentCover = new Animated.Value(0);
-    this.contentCoverScale = new Animated.Value(1);
-    this.interpolateTop = this.contentCover.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0, -1000]
-    });
-    this.interpolateColor = this.contentCover.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['rgb(0,0,0)', 'rgb(255,255,255)'],
-    });
-    this.interpolateColorReverse = this.contentCover.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['transparent', Colors.blue],
-    });
-
-    // TODO animate the color change from white to black
-
-    this.state = {
-      buttonDisabled: false,
-      buttonText: 'Show Resume',
-      hasRunInitial: false,
-      position: 'fixed'
-    }
-  }
-
-  show() {
-    const newState = {buttonDisabled: true, buttonText: 'Hide Resume'};
-    if (this.state.opacity === 0) {
-      newState.opacity = 1;
-    }
-    this.setState(newState);
-    Animated.spring(this.contentCover, {
-      toValue: 1,
-      tension: 2
-    }).start(() => this.setState({buttonDisabled: false, hasRunInitial: true, position: 'absolute'}))
-  }
-
-  hide() {
-    this.setState({buttonDisabled: true, buttonText: 'Show Resume', position: 'fixed'})
-    Animated.spring(this.contentCover, {
-      toValue: 0,
-      tension: 2
-    }).start(() => this.setState({buttonDisabled: false}))
-  }
-
-  handleClick = e => {
-    e.preventDefault();
-    const {buttonDisabled, position} = this.state;
-    if (buttonDisabled) {
-      return;
-    }
-    if (position === 'fixed') {
-      return this.show()
-    }
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-    return this.hide()
-  }
-
   render() {
-    const {buttonDisabled, buttonText, opacity, position} = this.state;
-    const buttonBackgroundColor = buttonDisabled ? Colors.greySecondary : Colors.blue
     return (
-      <div className={css(styles.container)} style={{opacity, position}}>
-        <Animated.div className={css(styles.containerMyInfo)} style={{color: this.interpolateColor}}>
-          <h1 className={css(styles.textMeMain)}>Dan Parker</h1>
-          <h2 className={css(styles.textMeSecondary)}>Developer</h2>
-        </Animated.div>
+      <div className={css(styles.container)}>
+        <div className={css(styles.imageBackground)}>
+          <h1 className={css(styles.textH1)}>Dan Parker</h1>
+          <p className={css(styles.textH2)}>Developer</p>
+        </div>
+        <div className={css(styles.containerContent)}>
+          <p className={css(styles.textH3)}>"I build elegant front end solutions and scalable, real-time backend services as a core member of Uber's Innovation Team."</p>
 
-        {/* <div className={css(styles.containerContent)}>
-          <Animated.div className={css(styles.containerLeftSide)} style={{backgroundColor: this.interpolateColorReverse}}>
-
-            <h2 className={css(styles.textLeftHeading)}>Highly Proficient With:</h2>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Redux</li>
-            <li>React-Native</li>
-            <li>GraphQL</li>
-            <li>Web Sockets</li>
-            <li>Sequelize</li>
-            <li>iOS App Certificates</li>
-            <li>Self Evaluation</li>
-            <li>Reciting Alphabet Backwards</li>
-
-            <h2 className={css(styles.textLeftHeading)}>Skilled In:</h2>
-            <li>Redis</li>
-            <li>PostgreSQL</li>
-            <li>MySQL</li>
-            <li>CSS</li>
-            <li>HTML</li>
-            <li>Teaching</li>
-            <li>Brewing Beer</li>
-
-            <h2 className={css(styles.textLeftHeading)}>Working Knowledge Of:</h2>
-            <li>Ruby</li>
-            <li>Rails</li>
-            <li>MongoDB</li>
-            <li>jQuery</li>
-            <li>Sinatra</li>
-            <li>Xcode</li>
-            <li>Android Studio</li>
-
-
-
-          </Animated.div>
-          <div className={css(styles.containerRightSide)}>
-            <div className={css(styles.containerRow)}>
-
-              <div className={css(styles.imageComputerDude)}/>
-              <div style={{display: 'flex', flexDirection: 'row', flex: 1}}>
-
-                <h1 className={css(styles.textRightHeading)}>I build elegant front end solutions and scalable, real-time backend services as a core member of Uber's Innovation Team.</h1>
-              </div>
+          <div className={css(styles.containerFooter)}>
+            <div className={css(styles.flexRow)}>
+              <Links />
             </div>
-
-            <h1 className={css(styles.textResumeHeading)}>About Me</h1>
-            <p className={css(styles.textResumeSubheading)}>{`I currently spend my time as Full Stack JavaScript developer at Uber in San Francisco.  I currently spend my time in a fast paced start-up environment within Uber.  I enjoy mentoring interns and Junior Developers on a daily basis.  I like to use new and exciting technologies in the projects I build and am lucky enough to build many new projects each year.  When not coding I seek fun in playing soccer, golf, travelling, brewing beer, and generally enjoying life.`}</p>
-
-            <h1 className={css(styles.textResumeHeading)}>Projects</h1>
-            <div
-              className={css(styles.containerRow)}
-              style={{flexWrap: 'wrap'}}
-            >
-
-              <Project name={'uView'}/>
-              <Project name={'Runners'}/>
-              <Project name={'Offline'}/>
-              <Project name={'Wallabee'}/>
-              <Project name={'Homescape'}/>
-              <Project name={'Musa Kickabll'}/>
-
-
+            <div className={css(styles.divider)}/>
+            <div className={css(styles.flexRow)}>
+              <Skills />
             </div>
-
-            <p>Kiosk</p>
-            <p>A little something about Kiosk with some sexy numbers</p>
-
-            <h1 className={css(styles.textResumeHeading)}>Work History</h1>
-            <p>That basic ass boring shit</p>
-
-            <h1 className={css(styles.textResumeHeading)}>TODO's</h1>
-            <p>LinkedIn Link</p>
-            <p>Medium Link</p>
-            <p>Github Link</p>
-            <p>Maybe Stack Overflow profile link</p>
-
           </div>
 
-        </div> */}
-
-
-
-        <Animated.div className={css(styles.imageBackground)} style={{top: this.interpolateTop, transform: [{scale: this.contentCoverScale}]}} />
-        <button
-          className={css(styles.button)}
-          onClick={this.handleClick}
-          style={{backgroundColor: buttonBackgroundColor}}
-          >
-          {buttonText}
-        </button>
+        </div>
       </div>
     );
   }
 }
 
-const standardMargin = '20px';
+const secondary = '#241b24';
+// const secondary = '#2e1d2d';
+
+
 
 const styles = StyleSheet.create({
-  button: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-    padding: '10px',
-    borderRadius: '5px',
-    color: Colors.white,
-    textAlign: 'center',
-    width: '100px'
-  },
   container: {
     display: 'flex',
     flex: 1,
     height: '100%',
     width: '100%',
+    flexDirection: 'column',
   },
   containerContent: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'row',
+    padding: '0% 5% 5% 5%',
   },
-  containerMyInfo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    zIndex: 1,
-    margin: `${standardMargin} 0 0 ${standardMargin}`,
-  },
-  containerLeftSide: {
-    // backgroundColor: Colors.blue,
-    color: Colors.white,
-    width: '400px',
-    padding: `150px 0px 200px ${standardMargin}`,
-    boxShadow: `2px 2px 1px 1px ${Colors.greySecondary}`
-  },
-  containerRightSide: {
-    flex: 1,
-    padding: standardMargin
-  },
-  containerRow: {
+  containerFooter: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center',
+    flex: 1,
+
+    '@media (max-width: 1150px)': {
+      flexDirection: 'column',
+    },
+  },
+
+  divider: {
+    display: 'none',
+    height: '0px',
+
+    '@media (max-width: 1150px)': {
+      display: 'block',
+      height: '40px'
+    },
   },
 
   imageBackground: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
     backgroundImage: `url(${Macbook})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center center',
-    height: '100%',
+    height: 700,
+    padding: '2% 5%',
+
+    '@media (max-width: 600px)': {
+      height: 500,
+      width: '100%',
+      padding: '2% 5%'
+    },
   },
-  imageComputerDude: {
-    backgroundImage: `url(${ComputerDude})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center center',
-    height: '200px',
-    width: '200px',
-    borderRadius: '100px',
-    border: `1px solid ${Colors.greySecondary}`,
-    margin: '0px 30px 0px 0px'
+  imageLarge: {
+    height: '88px',
+    width: '88px',
+    margin: '0px 60px 10px 0px',
+
+    '@media (max-width: 1350px)': {
+      margin: '0px 40px 10px 0px',
+    },
+
+    '@media (max-width: 600px)': {
+      height: '70px',
+      width: '70px',
+      margin: '0px 3px 10px 0px',
+    },
+  },
+  imageSmall: {
+    height: '40px',
+    width: '40px',
+    margin: '0px 14px 7px 0px',
+
+    '@media (max-width: 600px)': {
+      height: '40px',
+      width: '40px'
+    },
   },
 
+  flexRow: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
 
-  textMeMain: {
-    fontSize: '50px'
+    '@media (max-width: 600px)': {
+      justifyContent: 'space-between',
+    }
   },
-  textMeSecondary: {
+
+  textH1: {
+    fontFamily: 'mes-rg',
+    fontSize: '100px',
+    '@media (max-width: 600px)': {
+      fontSize: '65px',
+    }
+  },
+  textH2: {
+    color: secondary,
+    fontFamily: 'mes-lt',
+    fontSize: '80px',
+    '@media (max-width: 600px)': {
+      fontSize: '50px',
+    }
+  },
+  textH3: {
+    color: secondary,
+    fontFamily: 'mes-lt',
     fontSize: '40px',
-    margin: '10px 0px'
-  },
-  textLeftHeading: {
-    margin: '20px 0px 10px 0px'
-  },
-  textRightHeading: {
     textAlign: 'center',
-    color: Colors.black,
-    fontSize: '42px'
+    margin: '80px 100px',
+
+    '@media (max-width: 1150px)': {
+      fontSize: '30px',
+      margin: '60px 40px',
+    },
+
+    '@media (max-width: 600px)': {
+      fontSize: '23px',
+      margin: '40px 20px'
+    }
   },
-  textResumeHeading: {
-    color: Colors.black,
-    fontSize: '30px',
-    margin: '30px 0px 10px 0px'
-  },
-  textResumeSubheading: {
-    color: Colors.black,
-    fontSize: '20px',
-    whiteSpace: 'pre-line'
-    // margin: '30px 0px 10px 0px'
-  }
+
+
 })
